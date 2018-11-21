362dd0f70fc2dace0c2895039dfef7dd66fd3122
Willy Lyle <willysanderslyle@gmail.com>
2018-11-21 13:58:38 -0800

    work

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://duckhunt2.herokuapp.com/'
+    origins 'https://duckhunt2.herokuapp.com'
 
     resource '*',
       headers: :any,
